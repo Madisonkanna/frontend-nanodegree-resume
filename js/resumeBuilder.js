4a19e5523c32985ec5a0bdfa42f56577c047dfe9
madison kanna <madisonkanna@gmail.com>
2016-12-30 17:33:21 -0800

    Add bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,5 +8,18 @@ var formattedRole = HTMLheaderRole.replace("%data%", role)
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-
-
+var bio = {
+	"name" : "Madison Kanna",
+	"role": "Web Developer"
+	"contacts": {
+		"mobile": "916-599-5151",
+		"email": "madisonkanna",
+		"github": "madisonkanna",
+		"twitter": "@madisonkanna",
+		"location:" "Sacramento", 
+	},
+	"welcomeMessage": "Welcome to my online resume!"
+	"skills": [
+		"awesomeness", "JS", "HTML and CSS"]
+	"bioPic": "images/image.jpeg"
+}
