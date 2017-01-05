da2416139a2c93690326ddeccdf0cf0623878eea
madison kanna <madisonkanna@gmail.com>
2017-01-05 14:24:05 -0800

    Add skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,13 +6,32 @@ var role = "Web Developer";
 
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+$("#header").append(skills);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+var bio = {
+	"name" : "John Doe",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "650-555-5555",
+		"email": "john@example.com",
+		"github": "johndoe",
+		"twitter": "@johndoe",
+		"location": "San Francisco"
+	},
+	"welcomeMessage": "Welcome to my site!",
+	"skills": ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
+	"bioPic": "images/fry.jpg"
 
-var cameron = {};
+}
+
+$("#main").append(bio.role);
+
+
+cameron = {};
 cameron.job = "course dev";
 
 var makeCourse = function() {
